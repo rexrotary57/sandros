a5381182043e4891cbedd733cfab24a3538be12d
ortex <93113622+rexrotary57@users.noreply.github.com>
2023-07-30 20:42:36 +0330

    Update index.js
    update contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xbe12ba813978aed9c3eec2c66b18af8d506e849a";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
